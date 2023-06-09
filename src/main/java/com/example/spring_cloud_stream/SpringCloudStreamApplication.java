b15e07f2015479962465b2be2efe8e366b47466d
Igor Entaev <entaev@mail.ru>
2023-06-09 18:50:43 +0800

    проверка сообщений

--- src/main/java/com/example/spring_cloud_stream/SpringCloudStreamApplication.java
+++ src/main/java/com/example/spring_cloud_stream/SpringCloudStreamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCloudStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudStreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/spring_cloud_stream/SpringCloudStreamApplicationTests.java
+++ src/test/java/com/example/spring_cloud_stream/SpringCloudStreamApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringCloudStreamApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
